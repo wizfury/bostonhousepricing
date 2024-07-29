7a6d3026d58f6131ef1107512480473da453bbcc
wizfury <21052721@kiit.ac.in>
2024-07-30 02:38:25 +0530

    fix: standard scaler preprocessing

--- app.py
+++ app.py
@@ -20,13 +20,23 @@ def home():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.json["data"]
-    print(data)
-    print(np.array(list(data.values())[0]).reshape(1,-1))
+    print(np.array(list(data.values())).reshape(1,-1))
     new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    print(new_data)
     output = regmodel.predict(new_data)
     print(output[0])
     
     return jsonify(output[0])
+
+
+@app.route('/predict',methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text="The House price Prediction is: {}".format(output))
+    
     
     
 if __name__ == "__main__":
